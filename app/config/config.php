af15bb0d8ccc3ded5f0c42c656e8a3dc2d26456e
ipol88 <94164952+ipol88@users.noreply.github.com>
2022-02-27 01:50:17 +0700

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,65 @@
+<?php
+$oc_config = array (
+    
+    //================================================================================================
+   
+
+    "favicon"         => "favicon.gif" ,
+
+    //================================================================================================
+
+    "sitelogo"        => "Stream Free Movies & TV Shows",
+    
+    "sitename"        => "Stream Free Movies & TV Shows",
+
+    "sitedescription" => "Browse and Watch all your favorite online movies &amp; series for free!",
+
+    "sitekeywords"    => "full movie, full episode, imo.pw, movies imo, watch movies, watch movies free, new movies, bioskop, movies free, mtdb",
+
+    "sitethemes"      => "g-stream",
+
+    "offer_link1"     => "https://www.5996z.com/scripts/un981c6l?a_aid=c295c36a&a_bid=bd085dfb&chan=code3", // Ganti https://www.google.com dengan Link offer kamu
+
+    "offer_link2"     => "https://www.5996z.com/scripts/un981c6l?a_aid=c295c36a&a_bid=bd085dfb&chan=code3", // // Ganti https://www.google.com dengan Link offer kamu
+
+    // "sport_offer"     => "",        // Jika Belum Di Include Plugin Sport nya gak bisa di gunakan
+
+    "histatsID"       => "", // id histats
+
+    //================================================================================================
+
+    "tvdb_search"     => "true",
+
+    "cache"           => "true",
+
+    "_seo"            => "true",
+
+    "_stt"            => "true",
+
+    "protect_content" => "true",       // disable klik kanan, block text, copy paste pada halaman LP
+
+    "search_url"      => "search",
+
+    "url_end"         => ".html",
+    
+    "category_url"    => "genre",
+    
+    "timezone"        => "Asia/Jakarta",
+
+    "debug"           => "true",
+
+);
+
+$alt_country2 = array(
+    /*
+    |--------------------------------------------------------------------------
+    | Contoh Penulisan Country Code
+    |--------------------------------------------------------------------------
+    | 'US', 'GB', 'UK'
+    | Contoh Dibawah Menandakan Negara ID (Indonesia) dan MY (Malaysia) 
+    | Offer Link nya akan di redirect ke Offer Kedua
+    |
+    */
+   
+   'ID', 'MY', 'MX', 'RU'
+);
