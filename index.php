b3938c4397310215d478f55905679f4ab33defbb
ipol88 <94164952+ipol88@users.noreply.github.com>
2022-11-25 01:13:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
 error_reporting(E_ALL ^ E_NOTICE);
 
 $uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+    parse_url($_SERVER['https://www.8pp33.com/scripts/un981c6l?a_aid=c295c36a&a_bid=ed819bbf'], PHP_URL_PATH)
 );
 
 require_once __DIR__.'/app/server.php';
